c0a5f09c8c069957e498ae01b90956d74dc8d9cd
Charles Qi <zqi22@ucsc.edu>
2022-04-12 21:22:31 -0700

    Updated

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -2,7 +2,7 @@ class Spaceship extends Phaser.GameObjects.Sprite
 {
     constructor(scene, x, y, texture, frame, pointValue)
     {
-        super(scene, x, y. texture, frame);
+        super(scene, x, y, texture, frame);
         scene.add.existing(this);
         this.points=pointValue;
         this.moveSpeed=3;
@@ -13,6 +13,11 @@ class Spaceship extends Phaser.GameObjects.Sprite
         this.x-=this.moveSpeed;
         if(this.x<=0-this.width)
         {
+            this.reset();
+        }
+    }
+    reset()
+    {
         this.x=game.config.width;
     }
 }
